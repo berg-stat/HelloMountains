8e7f5e59faaf14a25a3ac697fcac1ca2f83aab62
weronka341 <36929275+weronka341@users.noreply.github.com>
2019-03-16 18:22:29 +0100

    Add colors palette

---
+++ consts/colors.js
@@ -0,0 +1,10 @@
+export default COLOR = {
+    BRICK_RED: '#DE4756',
+    DARK_SLATE_GRAY: '#191D2C',
+    DIM_GRAY: '#77445E',
+    LIGHT_DIM_GRAY: '#E3D9DE',
+    LIGHT_SLATE_GRAY: '#454655',
+    SLATE_GRAY: '#5A5D6D',
+    TEXT: '#DEE2EC',
+    WHITE: '#FFF'
+}
